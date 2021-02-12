7619200fdbdc99ae2bef7f62bf7fd2947133ce85
daveavi <avidave97@gmail.com>
2021-02-11 21:33:24 -0500

    added drawer

--- my-personal-website/src/App.js
+++ my-personal-website/src/App.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button'
-import SaveIcon from '@material-ui/icons/Save'
-import Checkbox from '@material-ui/core/Checkbox'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-
-import {makeStyles, ThemeProvider, createMuiTheme} from '@material-ui/core/styles'
-
 import AppBar from '@material-ui/core/AppBar';
 import MenuIcon from "@material-ui/icons/Menu";
 import ToolBar from "@material-ui/core/Toolbar"
@@ -14,6 +7,9 @@ import Typography from '@material-ui/core/Typography';
 
 import 'fontsource-roboto' 
 import { IconButton } from '@material-ui/core';
+import {makeStyles, ThemeProvider, createMuiTheme} from '@material-ui/core/styles'
+
+
 const useStyles = makeStyles((theme) => ({
   rightToolbar: {
     marginLeft: "auto",
@@ -24,6 +20,12 @@ const useStyles = makeStyles((theme) => ({
     marginRight:16,
     marginLeft: -12
   },
+  hide: {
+    display: 'none'
+  },
+  drawer:{
+    
+  }
 }));
 
 const theme =  createMuiTheme({
@@ -37,26 +39,6 @@ const theme =  createMuiTheme({
 
 
 
-function CheckboxExample(){
-  const[checked, setChecked] = React.useState(true)
-  return (
-    <div>
-    <FormControlLabel
-      control = {<Checkbox 
-        checked={checked}
-        icon = {<SaveIcon/>}
-        checkedIcon = {<SaveIcon/>}
-        onChange = {(e) => setChecked(e.target.checked)}
-        color="primary"
-        inputProps={{
-          'aria-label': 'secondary checkbox'
-        }}
-      />}
-      label = "Testing Checkbox"
-    />
-  </div>
-)
-}
 
 
 function App() {
@@ -66,12 +48,13 @@ return (
   <ThemeProvider theme={theme}>
       <div className="App">
         <header className="App-header">
-        <AppBar style={{ background: '#282c34' }} position="fixed">
+        <AppBar style={{ background: 'black' }} position="fixed">
           <ToolBar>
             <IconButton
               className = {classes.menuButton}
               aria-label="Menu"
               color="inherit"
+              onClick={handleDrawerOpen}
             >
               <MenuIcon/>
             </IconButton>
