7869440b22fb98774157193b5065cbc505a63c16
daveavi <avidave97@gmail.com>
2021-02-22 15:27:28 -0500

    have span now fixed in position

--- my-personal-website/src/components/menu-items/about/css/styles.js
+++ my-personal-website/src/components/menu-items/about/css/styles.js
@@ -1,20 +1,24 @@
 import { makeStyles } from '@material-ui/core/styles'
+import { CenterFocusStrong } from '@material-ui/icons';
 
 
 const useStyles = makeStyles((theme) => ({
     aboutTitle:{
         position: 'absolute',
         fontSize: 40,
-        top: '80px'
+        top: '80px',
     },
     aboutDesc: {
+        position: "absolute",
         fontSize: 20, 
-        // left: 20,
-        // top: '80px',
+        top: '250px',
         wordSpacing: '10px',
-        backgroundColor: 'red'
-        // marginLeft:'400px'
-        // bottom:'120000px'    
+        backgroundColor: 'red',
+        left: "1000px",
+        right: "20px",
+        height: "300px", 
+
+
 
     }
 }))
